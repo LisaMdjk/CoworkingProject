394cb1735db7b77529c19c86a7f604d79d4d397b
Lisa <lisamdjk@petit-macro.local>
2021-01-11 18:31:57 +0100

    more test with rufus

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,16 +4,14 @@ scheduler = Rufus::Scheduler.new
 
 # solution find on https://stackoverflow.com/questions/20490401/how-to-get-rufus-scheduler-working-with-a-rails-app-deployed-to-heroku
 #lets try
-s = Rufus::Scheduler.singleton
-s.every '1m' do
-  Rails.logger.info "hello, it's #{Time.now}"
-  Rails.logger.flush
-end
+#s = Rufus::Scheduler.singleton
+#s.every '1m' do
+#  Rails.logger.info "hello, it's #{Time.now}"
+#  Rails.logger.flush
+#end
 
-scheduler.every '1d' do
+scheduler.every '1m' do
+  puts "Checking requests confirmation..."
   RequestslistController.refresh
-end
-
-scheduler.every '1min' do
-  puts "IS RUFUS WORK???? yes if you see that"
+  puts "Checking DONE!"
 end
